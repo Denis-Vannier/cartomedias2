9493c1564b9b005cb8b227a69053d07f32f5c46c
Denis Vannier <Denis-Vannier@users.noreply.github.com>
2020-12-12 17:53:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const config = {
     zoom: 5, //Default zoom
     title: "Les rédactions locales en France",
     description: "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
-    sideBarInfo: ["Media", "Commune", "Site_web"],
+    sideBarInfo: ["Media", "COMMUNE", "Site_web"],
     popupInfo: ["Media"],
     filters: [
         {
