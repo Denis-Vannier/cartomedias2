ca72a7780eaae2f721e5b5fa9a74e1627c32ebed
Denis Vannier <Denis-Vannier@users.noreply.github.com>
2020-12-12 17:08:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ const config = {
     CSV: "./medias.csv",
     center: [2.778088, 47.201715], //Lng, Lat
     zoom: 5, //Default zoom
-    title: "L'info locale en France",
+    title: "Les rédactions locales en France",
     description: "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
-    sideBarInfo: ["Header", "Info 1", "Info 2"],
+    sideBarInfo: ["Media", "Commune", "Site_web"],
     popupInfo: ["Popup Information"],
     filters: [
         {
