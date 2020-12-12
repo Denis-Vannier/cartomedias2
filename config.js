d66ff09cc6730944479a7a545fbbe2b63c4ed823
Denis Vannier <Denis-Vannier@users.noreply.github.com>
2020-12-12 17:21:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = {
     title: "Les rédactions locales en France",
     description: "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
     sideBarInfo: ["Media", "Commune", "Site_web"],
-    popupInfo: ["Popup Information"],
+    popupInfo: ["Media"],
     filters: [
         {
             type: "dropdown",
